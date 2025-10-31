bfe446919372e36a5dbd3719f1d283dc12d508fc
saadyaq <saadyaqine91@gmail.com>
2025-10-31 13:28:09 +0100

    Parsing code

--- src/parser.py
+++ src/parser.py
@@ -46,12 +46,27 @@ def extract_classes(tree):
                 'methods':[m.name for m in node.body if isinstance(m,ast.FunctionDef)],
                 'docstring': ast.get_docstring(node)})
     return classes
-def extract_imports(tree):
-    """Extract all classes from an ast"""
 
+def extract_imports(tree):
+    """extract all import from an ast"""
+    imports = []
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for alias in node.names:
+                imports.append(alias.name)
+        elif isinstance(node, ast.ImportFrom):
+            module = node.module or ''
+            for alias in node.names:
+                imports.append(f"{module}.{alias.name}")
+    return imports
 
 def main():
-    parser()
+    file_path="/home/saadyaq/SE/Python/code_review_ai/test.py"
+    tree=parser(file_path)
+    classes=extract_classes(tree)
+    functions=extract_functions(tree)
+    imports=extract_imports(tree)
+    print(f"classes{classes}, functions{functions},imports{imports}")
 
 if __name__=="__main__":
     main()
