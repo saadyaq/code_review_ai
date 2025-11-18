c565c95af85b3d393d71db506d19a3c0192e2935
saadyaq <saadyaqine91@gmail.com>
2025-11-18 16:34:24 +0100

    Testing the github client

--- src/github_integration.py
+++ src/github_integration.py
@@ -5,3 +5,10 @@ def get_github_client():
     """ Create an authentified github client"""
     token=os.getenv("GITHUB_TOKEN")
     return Github(token)
+
+def main():
+    if get_github_client():
+        print(1)
+
+if __name__=="__main__":
+    main()
